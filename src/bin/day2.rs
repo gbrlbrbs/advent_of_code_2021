ecc4b5fdda16aca19e90d6de3838cbe688ca17da
Gabriel Martinz <gabrielbarbosamartinz_@outlook.com>
2021-12-25 10:36:46 -0300

    day 2 part 2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -7,17 +7,41 @@ fn main() {
         let path_str = filepath.to_str()
             .expect("Error converting path to string");
         let file_str = aoc::read_file_to_string(path_str);
-        let mut forward = 0;
-        let mut down = 0;
-        for line in file_str.lines() {
-            let dir = aoc::day2::find_direction(line);
-            match dir {
-                Directions::Forward(x) => forward += x,
-                Directions::Down(y) => down += y
-            };
-        }
+        let (forward, down) = part1(&file_str);
         println!("Submarine is in position ({}, {}), which multiplied is {}", forward, down, forward * down);
+        let (forward, down) = part2(&file_str);
+        println!("Actual position accounting for aim: ({}, {}), which multiplied is {}", forward, down, forward * down);
     } else {
         println!("File does not exist!");
     }
+}
+
+fn part1(file_str: &String) -> (i32, i32) {
+    let mut forward = 0;
+    let mut down = 0;
+    for line in file_str.lines() {
+        let dir = aoc::day2::find_direction(line);
+        match dir {
+            Directions::Forward(x) => forward += x,
+            Directions::Down(y) => down += y
+        };
+    }
+    (forward, down)
+}
+
+fn part2(file_str: &String) -> (i32, i32) {
+    let mut forward = 0;
+    let mut aim = 0;
+    let mut down = 0;
+    for line in file_str.lines() {
+        let dir = aoc::day2::find_direction(line);
+        match dir {
+            Directions::Forward(x) => {
+                forward += x;
+                down += aim * x
+            },
+            Directions::Down(y) => aim += y,
+        };
+    }
+    (forward, down)
 }
