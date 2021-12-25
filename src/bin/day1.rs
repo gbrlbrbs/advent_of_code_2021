68ca6efcb605a640af7c8d7eac228000b54bb3de
Gabriel Martinz <gabrielbarbosamartinz_@outlook.com>
2021-12-25 10:16:55 -0300

    day 1 pt 2 and better code

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -7,20 +7,42 @@ fn main() {
             .expect("Error getting path");
         let file_str = aoc::read_file_to_string(path_str);
         let numbers = aoc::read_string_separate_lines(&file_str);
-        let mut number_vec: Vec<i32> = numbers.into_iter()
+        let number_vec: Vec<i32> = numbers.into_iter()
             .map(|num| {
                 num.parse::<i32>().unwrap()
         }).collect();
-        let mut counter = 0;
-        while !number_vec.is_empty() {
-            let last = number_vec.pop().expect("Vector emptied unexpectedly");
-            if !number_vec.is_empty() && 
-                (*(number_vec.last().expect("Vector exhausted unexpectedly")) < last) {
-                counter += 1;
-            }
-        }
+        let counter = single_changes(&number_vec);
+        let counter_windows = sliding_window(&number_vec, 3);
         println!("There are {} positive changes in depth.", counter);
+        println!("There are {} positive sliding window changes in depth", counter_windows);
     } else {
         println!("File does not exist!");
     }
+}
+
+fn single_changes(vec: &Vec<i32>) -> i32 {
+    let mut iter = vec.iter();
+    let mut prev = *iter.next().expect("Empty vector!");
+    let mut counter = 0;
+    for item in iter {
+        if *item > prev {
+            counter += 1;
+        }
+        prev = *item;
+    }
+    counter
+}
+
+fn sliding_window(vec: &Vec<i32>, window_size: usize) -> i32 {
+    let new_vec = vec.clone();
+    let mut windows = new_vec.windows(window_size).peekable();
+    let mut prev = windows.next().expect("Empty vector!");
+    let mut counter = 0;
+    for item in windows {
+        if item.into_iter().sum::<i32>() > prev.into_iter().sum() {
+            counter += 1;
+        }
+        prev = item.clone();
+    }
+    counter
 }
